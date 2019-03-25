3bf43cf4c2f88ed69573e6c094b1101bc2ee3489
TJbaobao <1029291429@qq.com>
2019-03-25 18:40:17 +0800

    1、修改包名

--- app/src/androidTest/java/com/tjbaobao/gitee/googlebillingutil/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tjbaobao/gitee/googlebillingutil/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.tjbaobao.gitee.googlebuillingutil", appContext.packageName)
+        assertEquals("com.tjbaobao.gitee.googlebillingutil", appContext.packageName)
     }
 }
