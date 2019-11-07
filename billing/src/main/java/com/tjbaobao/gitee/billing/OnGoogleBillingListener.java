a7b6f682a93279cafa713175dcda0b46ad95c52e
TJHello <1029291429@qq.com>
2019-11-07 19:04:12 +0800

    1、添加Apache License2.0协议。

--- billing/src/main/java/com/tjbaobao/gitee/billing/OnGoogleBillingListener.java
+++ billing/src/main/java/com/tjbaobao/gitee/billing/OnGoogleBillingListener.java
@@ -8,10 +8,23 @@ import com.android.billingclient.api.SkuDetails;
 import java.util.List;
 
 /**
- * 作者:TJbaobao
+ * 作者:天镜baobao
  * 时间:2019/6/2  13:51
- * 说明:
+ * 说明:允许使用，但请遵循Apache License 2.0
  * 使用：
+ * Copyright 2019 天镜baobao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class OnGoogleBillingListener {
 
